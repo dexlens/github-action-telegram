0d86495f61b2c5814f5f26a1958327904a8e6bae
Liquid <13621351+liqtags@users.noreply.github.com>
2024-10-28 00:50:18 +0000

    Update main.ts

--- main.ts
+++ main.ts
@@ -20,12 +20,18 @@ if (import.meta.main) {
   const htmlURL = context.payload.repository?.html_url;
   const applicationVersion = denoJSON.version;
   const telegramChannel = Deno.args[1];
+  const pr1 = commitMerge.message.split(" ")[3].replace("#", "");
+  let repoLink = htmlURL;
+  let prLink = `${repoLink}/pull/${prNumber}`;
 
   bot.api.sendPhoto(telegramChannel, `https://banners.beyondco.de/${description}.png?theme=dark&packageManager=npm+install&packageName=@${fullname}&pattern=charlieBrown&style=style_1&description=&md=1&showWatermark=0&fontSize=150px&images=code`, {
     caption: commitMerge.message,
     reply_markup: {
       inline_keyboard: [
-        [{ text: "View on GitHub", url: htmlURL as string }],
+        [
+          { text: "View Repo", url: htmlURL as string },
+          { text: "View Pull Request", url: prLink as string }
+        ],
       ],
     },
   });
