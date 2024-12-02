6d03deb21ba3d357d1b3d75a9434437456838612
Liquid <13621351+liqtags@users.noreply.github.com>
2024-12-02 16:28:17 +0000

    add

--- main.ts
+++ main.ts
@@ -95,10 +95,10 @@ if (import.meta.main) {
       parse_mode: "HTML",
     });
   } else {
-    let entryProjectData = Deno.readTextFileSync(`../${branchName}/project.json`);
-    let projectData = JSON.parse(entryProjectData);
+    // let entryProjectData = Deno.readTextFileSync(`../${branchName}/project.json`);
+    // let projectData = JSON.parse(entryProjectData);
     console.log(`I am grabbing this image: https://raw.githubusercontent.com/dexlens/dehub/refs/heads/main/${branchName}/${branchName}.png`);
-    console.log("Project Data: ", projectData);
+    // console.log("Project Data: ", projectData);
 
     // bot.api.sendPhoto(telegramChannel, `https://raw.githubusercontent.com/dexlens/dehub/refs/heads/main/${branchName}/${branchName}.png`, {
     //   caption: htmlCaption,
